67951a69ab696f0fcc3df0b26eaf223ce4a49b6d
liangzl14 <liangzl14@chinaunicom.cn>
2019-12-16 20:08:44 +0800

    配置文件，pom文件，生成model dao mapper文件

---
+++ src/main/java/com/onlineretailers/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.onlineretailers.dao;
+
+import newmodels.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
